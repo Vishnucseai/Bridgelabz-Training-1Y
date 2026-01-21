84cbeb00dd9999f3c0b865e9a68766dc4894d95b
vishnu <vishnu2005.krishna@gmail.com>
2026-01-21 21:49:02 +0530

    [Vishnu] 02-Java-Primitive-Datatype

---
+++ basic-programs/02-Java-Primitive-Data-Type/Age01.java
@@ -0,0 +1,9 @@
+class Age
+{
+	public static void main(String[] args) {
+	    int dob = 2000;
+	    int currentyear = 2024;
+	    
+		System.out.println("Harry's age in 2024 is "+(currentyear-dob));
+	}
+}
---
+++ basic-programs/02-Java-Primitive-Data-Type/AverageMarks02.java
@@ -0,0 +1,10 @@
+public class AverageMarks
+{
+	public static void main(String[] args) {
+	    double physics = 95;
+	    double chemistry = 96;
+	    double maths = 94;
+	    double avgpercentage = ((maths + chemistry + physics)/300)*100;
+		System.out.println("Sam’s average mark in PCM is "+avgpercentage);
+	}
+}
