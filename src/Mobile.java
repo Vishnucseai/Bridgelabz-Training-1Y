3301b072e3d61c50a4d0e9d204d27720ddfff458
Vishnu <220510404+Vishnucseai@users.noreply.github.com>
2026-03-15 23:39:59 +0530

    [Vishnu]Files

---
+++ src/Mobile.java
@@ -0,0 +1,14 @@
+class Mobile {
+    String name = "iphone";
+    String Model = "Vivo";
+    String Battery = "5500Mah";
+
+
+
+    public static void main(String[] args) {
+        Mobile m = new Mobile();
+        System.out.println(m.name);
+        System.out.println(m.Model);
+        System.out.println(m.Battery);
+    }
+}
